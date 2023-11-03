bef11a4fcb7a50e2dffb1353474753660cefb76d
Eduardo Varela <eduardovarela139@gmail.com>
2023-11-03 15:09:06 -0600

    Astro tutorial finished

---
+++ src/components/gretting.tsx
@@ -0,0 +1,20 @@
+import { useState } from "preact/hooks";
+
+interface Props {
+    messages: string[];
+}
+
+export const Greeting = ({ messages }: Props) => {
+    const [greeting, setGreeting] = useState<string>(messages[0]);
+
+    const randomMessage = () => messages[Math.floor(Math.random() * messages.length)];
+
+    return (
+        <div>
+            <h3>{greeting}! Thank you for visiting!</h3>
+            <button onClick={() => setGreeting(randomMessage)}>New Greeting</button>
+        </div>
+    );
+};
+
+Greeting.displayName = "Greeting";
